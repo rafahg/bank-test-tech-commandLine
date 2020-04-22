41983a280a91bf5cc8c62e160bdf4da3ffee7fc3
Rafael <r.hgar78@gmail.com>
2020-04-22 17:09:57 +0200

    operations.rb added

---
+++ lib/operations.rb
@@ -0,0 +1,22 @@
+require_relative 'print'
+require_relative 'bank'
+
+class Operations 
+  attr_reader :client, :account, :balance, :client_name
+
+  def initialize(client)
+    @account = { client => [] }
+    @client_name = client 
+    @balance = 0
+  end
+  
+  def deposit(date, amount)
+    @account[client_name].push(["credit", date, amount, @balance += amount])
+  end
+    
+  def withdrawal(date, amount)
+    @account[client_name].push(["debit", date, amount, @balance -= amount])
+  
+  end
+  
+end 
